1d2c554969663f0f347c2192b422d794c621bfbd
workstg <notice@uramiraikan.net>
2020-07-26 14:03:57 +0900

    "CreateUserFolder.ps1"を追加
     * フォルダリダイレクト用の個人フォルダ作成とアクセス権付与

---
+++ ps1/CreateUserFolder.ps1
@@ -0,0 +1,40 @@
+<#
+フォルダリダイレクト用フォルダの作成
+#>
+# Parameters
+$DomainBios = "DOMAIN"
+$TargetGroup = "Domain Users"
+$RootFolder = "\\127.0.0.1\Shared"
+
+# Main Routine
+$GroupUsers = Get-ADGroupMember -Identity $TargetGroup
+$Folders = Get-ChildItem -Path $RootFolder
+
+foreach ($DomainUser in $GroupUsers) {
+   $Stat = $true
+   foreach ($UserFolder in $Folders) {
+      if ($DomainUser.SamAccountName -ieq $UserFolder.Name) {
+         Write-Output "`"$UserFolder`" is already exist."
+         $Stat = $false
+      }
+   }
+   if ($Stat) {
+      $UserPath = $RootFolder + "\" + $DomainUser.SamAccountName
+      $AllowUser = $DomainBios + "\" + $DomainUser.SamAccountName
+      $UserRuleSet = (
+         $AllowUser,
+         [System.Security.AccessControl.FileSystemRights]::Modify,
+         ([System.Security.AccessControl.InheritanceFlags]::ObjectInherit -bor [System.Security.AccessControl.InheritanceFlags]::ContainerInherit),
+         [System.Security.AccessControl.PropagationFlags]::None,
+         [System.Security.AccessControl.AccessControlType]::Allow
+      )
+      $UserAcl = New-Object System.Security.AccessControl.FileSystemAccessRule $UserRuleSet
+
+      Write-Output "Create Folder : `"$UserPath`""
+      New-Item -Path $UserPath -ItemType Directory
+      $FolderAcl = Get-Acl $UserPath
+      $FolderAcl.SetAccessRule($UserAcl)
+      $FolderAcl | Set-Acl $UserPath
+   }
+}
+exit
