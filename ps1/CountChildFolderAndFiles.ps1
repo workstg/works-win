31b5b10e6061217265bbd93f9efd66ba0af593e8
workstg <notice@uramiraikan.net>
2020-08-16 22:15:55 +0900

    "CountChildFolderAndFiles.ps1"を変更
     * 数値の型を"int64"から"double"に変更
     * 指定パスの存在確認を追加

--- ps1/CountChildFolderAndFiles.ps1
+++ ps1/CountChildFolderAndFiles.ps1
@@ -6,16 +6,21 @@ $TargetPath = "\\127.0.0.1\Data"
 $ResultTable = @()
 
 # Main Routine
-$SubFolders = Get-ChildItem -Path $TargetPath -Force | Where-Object { $_.PSIsContainer }
+if (Test-Path -Path $TargetPath -PathType Container) {
+   $SubFolders = Get-ChildItem -Path $TargetPath -Force | Where-Object { $_.PSIsContainer }
+} else {
+   Write-Output "The folder not found!"
+   exit 1
+}
 
 foreach ($SubFolder in $SubFolders) {
    $Result = New-Object PSObject | Select-Object Name, NumFolders, NumFiles, TotalSize, TotalSizeMB
    $Result.Name = $SubFolder.Name
 
    $AllItems = Get-ChildItem -Path $SubFolder.FullName -Recurse -Force
-   $Result.NumFolders = [int64](($AllItems | Where-Object {$_.PsIsContainer}).Count)
-   $Result.NumFiles = [int64](($AllItems | Where-Object {! $_.PsIsContainer}).Count)
-   $Result.TotalSize = [int64](($AllItems | Measure-Object Length -Sum).Sum)
+   $Result.NumFolders = [double](($AllItems | Where-Object {$_.PsIsContainer}).Count)
+   $Result.NumFiles = [double](($AllItems | Where-Object {! $_.PsIsContainer}).Count)
+   $Result.TotalSize = [double](($AllItems | Measure-Object Length -Sum).Sum)
    $Result.TotalSizeMB = [math]::round(($AllItems | Measure-Object Length -Sum).Sum / 1MB, 2)
 
    $ResultTable += $Result
