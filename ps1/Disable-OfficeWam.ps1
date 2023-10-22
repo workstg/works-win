865b584d7ead7f752214fd172800b26f02171934
workstg <notice@uramiraikan.net>
2023-10-22 17:38:22 +0900

    "Disable-OfficeWam.ps1"を追加
     * M365 AppsのWAM認証を無効化するPowerShellスクリプト

---
+++ ps1/Disable-OfficeWam.ps1
@@ -0,0 +1,25 @@
+# Web Account Manager (WAM) is Authentication Framework
+$DisableWam = $true
+
+$RegPath = "HKCU:Software\Microsoft\Office\16.0\Common\Identity"
+$RegList = ("DisableADALatopWAMOverride", "DisableAADWAM", "DisableMSAWAM", "DisableADALSharedCache")
+
+if (! (Test-Path -LiteralPath $RegPath)) {
+   Write-Output "Microsoft 365 Apps is not installed!"
+   exit
+}
+If ($DisableWam) {
+   $IdentityProperty = Get-ItemProperty -LiteralPath $RegPath
+   foreach ($RegName in $RegList) {
+      if ($null -ne $IdentityProperty.$RegName) {
+         $Ret = Remove-ItemProperty -LiteralPath $RegPath -Name $RegName
+      }
+      $Ret = New-ItemProperty -LiteralPath $RegPath -Name $RegName -PropertyType 'DWord' -Value 1
+   }
+} else {
+   foreach ($RegName in $RegList) {
+      $Ret = Remove-ItemProperty -LiteralPath $RegPath -Name $RegName
+   }
+}
+Get-ItemProperty -LiteralPath $RegPath
+exit
